b32cd7fd71700cc93c2a39c267de2934ed6c0aad
theColburns.michael@gmail.com <theColburns.michael@gmail.com@297d0162-a161-3ea2-cc57-87bb5d688e91>
2015-01-10 16:05:47 +0000

    Trello B-2015-002

---
+++ net.ages.workbench.templates.dsl/src/net/ages/workbench/templates/dsl/utils/VersionManager.java
@@ -0,0 +1,90 @@
+package net.ages.workbench.templates.dsl.utils;
+
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.Map;
+
+/**
+ * For a given paragraph of text, handles the versions of the sids and rids.
+ * This class keeps a list of domains and a list of versions.
+ * The reason it keeps a list of domains is so the user can avoid a lookup
+ * for a given domain if it has already been done.
+ * @author mac002
+ *
+ */
+public class VersionManager {
+	LinkedList<String> versions = null;
+	Map<String,String> domains = null;
+	String prefix;
+	String suffix;
+	String delimiter;
+	
+	public VersionManager(String prefix, String suffix, String delimiter) {
+		this.prefix = prefix;
+		this.suffix = suffix;
+		this.delimiter = delimiter;
+		resetVersions();
+	}
+	
+	/**
+	 * Call this after calling the toHtmlSpan() if you
+	 * don't plan on using the same set of versions.
+	 */
+	public void resetVersions() {
+		domains = new HashMap<String,String>();
+		versions = new LinkedList<String>();
+	}
+
+	/**
+	 * Checks to see if the given domain exists
+	 * in the versions Map.  This should be called so that
+	 * unnecessary version lookups are not made.
+	 * @param domain
+	 * @return true if the domain exists
+	 */
+	public boolean contains(String domain) {
+		return domains.containsKey(domain);
+	}
+	
+	/**
+	 * Add the supplied domain and version
+	 * @param domain
+	 * @param version
+	 */
+	public void push(String domain, String version) {
+		// Note that a version can occur in multiple domains.
+		// But, we want a unique list of versions.
+		if (! domains.containsKey(domain)) {
+			domains.put(domain, version);
+			if (! versions.contains(version)) {
+				versions.add(version);
+			}
+		}
+	}
+	
+	
+	/**
+	 * Converts the values of the Version map to a delimited string
+	 * @param delimiter to use
+	 * @return string of values separated by the delimiter
+	 */
+	public String delimitedValues() {
+		if (domains.isEmpty()) {
+			return "";
+		} else {
+			StringBuffer sb = new StringBuffer();
+			Iterator<String> it = versions.iterator();
+			sb.append(prefix);
+			while (it.hasNext()) {
+				sb.append(it.next());
+				if (it.hasNext()) {
+					sb.append(delimiter + " ");
+				}
+			}
+			sb.append(suffix);
+			return sb.toString();			
+		}
+	}
+	
+}
