1af68a04a8827f8a915732a309027fb33fb2b780
Michael Colburn <mac002@theColburns.us>
2016-07-01 15:18:27 +0300

    Fix for clocking when hover over unresolved reference

--- net.ages.workbench.templates.dsl.ui/src/net/ages/workbench/templates/dsl/ui/quickfix/AtemQuickfixProvider.java
+++ net.ages.workbench.templates.dsl.ui/src/net/ages/workbench/templates/dsl/ui/quickfix/AtemQuickfixProvider.java
@@ -1,6 +1,6 @@
-
 package net.ages.workbench.templates.dsl.ui.quickfix;
 
+import org.eclipse.xtext.diagnostics.Diagnostic;
 import org.eclipse.xtext.ui.editor.quickfix.DefaultQuickfixProvider;
 import org.eclipse.jface.text.BadLocationException;
 import org.eclipse.xtext.ui.editor.model.IXtextDocument;
@@ -13,16 +13,25 @@ import org.eclipse.xtext.ui.editor.quickfix.Fix;
 
 import net.ages.workbench.templates.dsl.validation.AtemJavaValidator;
 
-
 public class AtemQuickfixProvider extends DefaultQuickfixProvider {
 
-	       @Fix(AtemJavaValidator.NAME_MISMATCH)
-	       public void fixName(final Issue issue, IssueResolutionAcceptor acceptor) {
-	            acceptor.accept(issue, "Rename template.", "Change template name to match filename.", null, new IModification() {
-	                   public void apply(IModificationContext context) throws BadLocationException {
-	                        IXtextDocument xtextDocument = context.getXtextDocument();
-	                        xtextDocument.replace(issue.getOffset(), issue.getLength(), issue.getData()[0]);
-	                  }
-	            });
-	      }
+	public void createLinkingIssueResolutions(final Issue issue,
+			final IssueResolutionAcceptor issueResolutionAcceptor) {
+	}
+
+	@Fix(AtemJavaValidator.NAME_MISMATCH)
+	public void fixName(final Issue issue, IssueResolutionAcceptor acceptor) {
+		acceptor.accept(issue, "Rename template.",
+				"Change template name to match filename.", null,
+				new IModification() {
+					public void apply(IModificationContext context)
+							throws BadLocationException {
+						IXtextDocument xtextDocument = context
+								.getXtextDocument();
+						xtextDocument.replace(issue.getOffset(),
+								issue.getLength(), issue.getData()[0]);
+					}
+				});
+	}
+
 }
